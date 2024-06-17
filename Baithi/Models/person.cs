1d04f2ba4c3273cf9de69d65ea33009b2175a32e
hoangngoctung <hoangngoctung22@gmail.com>
2024-06-17 13:23:46 +0700

    HNT705person

---
+++ Baithi/Models/person.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace HNT705 
+[Table("Person")]
+(
+    [key]
+    public string CCCD { get; set; }
+        public string HovaTen { get; set; }
+            public string DiaChi { get; set; }
+)
